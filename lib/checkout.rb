c4922a8fa5c5f489362038633fa6410301dd01b7
Josh Blumberg <jlblumberg@gmail.com>
2020-06-19 11:29:30 +0100

    Remove comment

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -11,17 +11,6 @@ class Shop
 
   def checkout(item)
     @values[item.to_sym].nil? ? -1 : @values[item.to_sym]
-    # if item == 'A'
-    #   50
-    # elsif item == 'B'
-    #   30
-    # elsif item == 'C'
-    #   20
-    # elsif item == 'D'
-    #   15
-    # else 
-    #   -1
-    # end
   end
 
 end
