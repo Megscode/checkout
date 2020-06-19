d054a425647f74123e1c374dc8f568d917d2fefa
Josh Blumberg <jlblumberg@gmail.com>
2020-06-19 11:26:55 +0100

    Refactor to use a hash instance variable

--- lib/checkout.rb
+++ lib/checkout.rb
@@ -1,17 +1,27 @@
 class Shop
 
+  def initialize
+    @values = {
+      A: 50,
+      B: 30,
+      C: 20,
+      D: 15
+    }
+  end
+
   def checkout(item)
-    if item == 'A'
-      50
-    elsif item == 'B'
-      30
-    elsif item == 'C'
-      20
-    elsif item == 'D'
-      15
-    else 
-      -1
-    end
+    @values[item.to_sym].nil? ? -1 : @values[item.to_sym]
+    # if item == 'A'
+    #   50
+    # elsif item == 'B'
+    #   30
+    # elsif item == 'C'
+    #   20
+    # elsif item == 'D'
+    #   15
+    # else 
+    #   -1
+    # end
   end
 
 end
